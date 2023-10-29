f9ed4c1df56b8c7a9db1050ae9b08ff9f10fa3fc
r151149 <45209359+r151149@users.noreply.github.com>
2023-10-29 11:37:17 +0530

    update server url

--- client/worker.js
+++ client/worker.js
@@ -1,57 +1,50 @@
 console.log(`service worker loaded...`);
 
+self.addEventListener("push", (e) => {
+  console.log("Worker::: ", e.data.json());
 
-self.addEventListener('push', e=>{
-    console.log("Worker::: ", e.data.json());
-    
-    const data = e.data.json();
+  const data = e.data.json();
 
-    var views = {id:data.data.notification_id, viewCount:1};
+  var views = { id: data.data.notification_id, viewCount: 1 };
 
-    console.log(`push received, views::: `, views);
+  console.log(`push received, views::: `, views);
 
-    // fetch('http://localhost:5001/updateViewsClicks',{
-        fetch('https://push-notifications-149.herokuapp.com/updateViewsClicks',{
-        method:'POST',
-        body:JSON.stringify({views}),
-        headers:{
-            'content-type':'application/json'
-        }
-    })
-    .then(res=>console.log(`updated views`));
+  // fetch('http://localhost:5001/updateViewsClicks',{
+  fetch("https://push-notifications-iota.vercel.app//updateViewsClicks", {
+    method: "POST",
+    body: JSON.stringify({ views }),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((res) => console.log(`updated views`));
 
-    
-    self.registration.showNotification(data.title, {
-        body: data.msg,
-        icon:data.icon,
-        badge:data.image,
-        image:data.image,
-        data:data.data
-    });
+  self.registration.showNotification(data.title, {
+    body: data.msg,
+    icon: data.icon,
+    badge: data.image,
+    image: data.image,
+    data: data.data,
+  });
 });
 
-self.addEventListener('notificationclick',async function(e) {
+self.addEventListener("notificationclick", async function (e) {
+  console.log(`onClick worker:::`, e.notification);
 
-    console.log(`onClick worker:::`,e.notification);
-    
-    const data = e.notification.data;
+  const data = e.notification.data;
 
-    var clicks = {id:data?.notification_id, clickCount:1};
-    console.log(`Clicked:::`, clicks);
+  var clicks = { id: data?.notification_id, clickCount: 1 };
+  console.log(`Clicked:::`, clicks);
 
-    e.notification.close();
+  e.notification.close();
 
-    e.waitUntil(
-        clients.openWindow(data?.url)
-    );
-    
-    // await fetch('http://localhost:5001/updateViewsClicks',{
-    await fetch('https://push-notifications-149.herokuapp.com/updateViewsClicks',{
-        method:'POST',
-        body:JSON.stringify({clicks}),
-        headers:{
-            'content-type':'application/json'
-        }
-    });
-    
-})
+  e.waitUntil(clients.openWindow(data?.url));
+
+  // await fetch('http://localhost:5001/updateViewsClicks',{
+  await fetch("https://push-notifications-iota.vercel.app//updateViewsClicks", {
+    method: "POST",
+    body: JSON.stringify({ clicks }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+});
